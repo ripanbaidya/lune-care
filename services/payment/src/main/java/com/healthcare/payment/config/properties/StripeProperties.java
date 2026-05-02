6aa42e5b88eeec7afefdf3d443fce249289f1c01
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-05-02 11:31:24 +0530

    fix (payment): Remove @RefreshScope from cofig properties file

--- services/payment/src/main/java/com/healthcare/payment/config/properties/StripeProperties.java
+++ services/payment/src/main/java/com/healthcare/payment/config/properties/StripeProperties.java
@@ -1,6 +1,7 @@
 package com.healthcare.payment.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties(prefix = "app.payment.gateway.stripe")
 public record StripeProperties(
