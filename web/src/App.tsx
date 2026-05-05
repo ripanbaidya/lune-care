7bb8600eb22e87c6f1c5890b3ed4fccd30adfac2
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-05-05 23:50:12 +0530

    feat(frontend): initialize React app with Tailwind CSS and TypeScript setup

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="h-screen bg-black flex items-center justify-center">
+            <h1 className="text-4xl text-yellow-400 font-bold">
+                Tailwind is working
+            </h1>
+        </div>
+    )
+}
